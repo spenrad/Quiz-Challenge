e3a2944fbd6b3c6cd37a08c81531a1d49bad09d1
Spencer Christy <spenrad@gmail.com>
2020-12-21 22:33:50 -0800

    added event listen for the start button, click start button engages the timer

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
 var quizHeader = document.querySelector("#quiz-header");
 var quizInfo = document.querySelector("#quiz-info");
-var buttonStart = document.querySelector("#button-start")
+var buttonStart = document.querySelector("#button-start");
 var quizAnswers = document.querySelector("#quiz-ansers");
 var score = document.querySelector("#time-left");
-var highScore = document.querySelector("#high-scores")
+var highScore = document.querySelector("#high-scores");
 
-var time = 0
+var time = 60
 
 function quizStart() {
-
+    var timeDown = setInterval(function() {
+        time--;
+        score.textContent = time
+    }, 1000);
 }
 
-buttonStart.addEventListener("click", function(event));
+buttonStart.addEventListener("click", quizStart());

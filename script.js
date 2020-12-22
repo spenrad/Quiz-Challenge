17da1fe95c6740529d8ff982bc54fef41cb77f37
Spencer Christy <spenrad@gmail.com>
2020-12-21 21:41:01 -0800

    added variables into js file

---
+++ script.js
@@ -0,0 +1,14 @@
+var quizHeader = document.querySelector("#quiz-header");
+var quizInfo = document.querySelector("#quiz-info");
+var buttonStart = document.querySelector("#button-start")
+var quizAnswers = document.querySelector("#quiz-ansers");
+var score = document.querySelector("#time-left");
+var highScore = document.querySelector("#high-scores")
+
+var time = 0
+
+function quizStart() {
+
+}
+
+buttonStart.addEventListener("click", function(event));

c19f370b460ed9457ca1b57dc97cb7529b8efb81
Spencer Christy <spenrad@gmail.com>
2020-12-22 01:24:34 -0800

    defined function to fetch answers from arrays and creat li elements within ul parent

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 var quizHeader = document.querySelector("#quiz-header");
 var quizInfo = document.querySelector("#quiz-info");
 var buttonStart = document.querySelector("#button-start");
-var quizAnswers = document.querySelector("#quiz-ansers");
+var quizAnswers = document.querySelector("#quiz-answers");
 var score = document.querySelector("#time-left");
 var highScore = document.querySelector("#high-scores");
 
@@ -19,14 +19,29 @@ function quizStart() {
         time--;
         score.textContent = time
     }, 1000);
-    if (quizInfo.style.visibility == "visible") {
+        if (quizInfo.style.visibility == "visible") {
         quizInfo.style.visibility = "hidden";
-    };
-    if (buttonStart.style.visibility == "visibile") {
+        };
+        if (buttonStart.style.visibility == "visibile") {
         buttonStart.style.visibility = "hidden";
-    };
-
+        };
 }
+//     getAnswers (answerBlock1);
+//     // getAnswers (answerBlock2);
+//     // getAnswers (answerBlock3);
+//     // getAnswers (answerBlock4);
 
 
-var quizStarted = buttonStart.addEventListener("click", quizStart());
+// function getAnswers(answerBlock) {
+//     quizAnswers.innerHTML = "";
+//     for (i = 0; i < answerBlock.length; i ++) {
+//         var answer = answerBlock[i];
+
+//         var li = document.createElement("li");
+//         li.textContent = answer;
+//         quizAnswers.appendChild(li);
+//     }
+// }
+
+
+buttonStart.addEventListener("click", quizStart());
